47afeadf7867c2a186e8ffa6e29e76985a543840
Vladimir Bauer <vbauerster@gmail.com>
2021-10-11 01:03:44 +0500

    two-sum: return Option<(i32, i32)>

--- two-sum/src/lib.rs
+++ two-sum/src/lib.rs
@@ -1,29 +1,32 @@
+// https://leetcode.com/problems/two-sum
 use std::collections::HashMap;
+use std::convert::TryFrom;
 
-pub fn two_sum_v1(nums: &[i32], target: i32) -> Vec<usize> {
-    let hm = nums.iter().zip(0..).collect::<HashMap<_, _>>();
+pub fn two_sum_v1(nums: &[i32], target: i32) -> Option<(i32, i32)> {
+    let map = nums.iter().zip(0..).collect::<HashMap<_, _>>();
     for (i, &n) in nums.iter().enumerate() {
-        let sub = target - n;
-        if let Some(&sub_index) = hm.get(&sub) {
-            if i == sub_index {
-                continue;
+        let i = i32::try_from(i).unwrap();
+        let look = target - n;
+        if let Some(&j) = map.get(&look) {
+            if i != j {
+                return Some((i, j));
             }
-            return vec![i, sub_index];
         }
     }
-    vec![]
+    None
 }
 
-pub fn two_sum_v2(nums: &[i32], target: i32) -> Vec<usize> {
-    let mut hm = HashMap::with_capacity(nums.len());
+pub fn two_sum_v2(nums: &[i32], target: i32) -> Option<(i32, i32)> {
+    let mut map = HashMap::with_capacity(nums.len());
     for (i, &n) in nums.iter().enumerate() {
-        let sub = target - n;
-        if let Some(&sub_index) = hm.get(&sub) {
-            return vec![i, sub_index];
+        let i = i32::try_from(i).unwrap();
+        let look = target - n;
+        if let Some(&j) = map.get(&look) {
+            return Some((j, i));
         }
-        hm.insert(n, i);
+        map.insert(n, i);
     }
-    vec![]
+    None
 }
 
 #[cfg(test)]
@@ -31,14 +34,20 @@ mod tests {
     use super::*;
     #[test]
     fn test_two_sum_v1() {
-        let nums = vec![2, 11, 7, 15];
-        let res = two_sum_v1(&nums, 9);
-        assert_eq!(nums[res[0]] + nums[res[1]], 9);
+        let nums = vec![2, 7, 11, 15];
+        assert_eq!(two_sum_v1(&nums, 9), Some((0, 1)));
+        let nums = vec![3, 2, 4];
+        assert_eq!(two_sum_v1(&nums, 6), Some((1, 2)));
+        let nums = vec![3, 3];
+        assert_eq!(two_sum_v1(&nums, 6), Some((0, 1)));
     }
     #[test]
     fn test_two_sum_v2() {
-        let nums = vec![2, 11, 7, 15];
-        let res = two_sum_v2(&nums, 9);
-        assert_eq!(nums[res[0]] + nums[res[1]], 9);
+        let nums = vec![2, 7, 11, 15];
+        assert_eq!(two_sum_v2(&nums, 9), Some((0, 1)));
+        let nums = vec![3, 2, 4];
+        assert_eq!(two_sum_v2(&nums, 6), Some((1, 2)));
+        let nums = vec![3, 3];
+        assert_eq!(two_sum_v2(&nums, 6), Some((0, 1)));
     }
 }
